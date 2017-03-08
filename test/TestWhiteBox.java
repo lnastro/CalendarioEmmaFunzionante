77ebeaf71302d97b492ef1fa17c28cd39f563f57
Luca Nastro <lnastro@codin.net>
2017-03-08 17:21:01 +0100

    modifica

--- test/TestWhiteBox.java
+++ test/TestWhiteBox.java
@@ -19,7 +19,7 @@ public class TestWhiteBox {
 	
 	@Test
 	public void testCalendWB1() {
-		ass.assertEquals(Calendario.calend(29,"gennaio",2011),"Sabat");
+		ass.assertEquals(Calendario.calend(29,"gennaio",2011),"Sabato");
 	}
 	@Test
 	public void testCalendWB2() {
